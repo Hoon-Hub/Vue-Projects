541493b900f0e7fa0bb59ce1fa0c21cfe948e1d2
Hoon-Hub <gwi01304@naver.com>
2021-09-20 00:42:01 +0900

    examples

---
+++ 03-number-baseball/src/NumberBaseball.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="root">
+        <h1>{{ result }}</h1>
+        <form v-on:submit="onSubmitForm">
+            <input ref="answer" maxlength="4" v-model="value" type="text">
+
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                value: '',
+            }
+        },
+        methods: {
+            onSubmitForm(e) {
+                e.preventDefault()
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
